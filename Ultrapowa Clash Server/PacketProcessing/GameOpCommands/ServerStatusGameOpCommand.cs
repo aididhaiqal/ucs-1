3217547ac61f404ca2ff055c26be1b9f06e3229e
iSwuerfel <tischlak_git@yahoo.de>
2015-10-29 16:42:22 +0100

    Added /Sysinfo Command

---
+++ Ultrapowa Clash Server/PacketProcessing/GameOpCommands/ServerStatusGameOpCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using UCS.Logic;
+using UCS.Helpers;
+using UCS.GameFiles;
+using UCS.Core;
+using UCS.Network;
+
+
+namespace UCS.PacketProcessing
+{
+    class ServerStatusGameOpCommand : GameOpCommand
+    {
+        private string[] m_vArgs;
+
+        public ServerStatusGameOpCommand(string[] args)
+        {
+            m_vArgs = args;
+            SetRequiredAccountPrivileges(1);
+        }
+
+        public override void Execute(Level level)
+        {
+            if (level.GetAccountPrivileges() >= GetRequiredAccountPrivileges())
+            {
+                if (m_vArgs.Length >= 1)
+                {
+                    //"Established Connections: " Maybe useless
+                    string message = string.Join("Established Connections: "  + "\n", m_vArgs.Skip(1));
+                    AllianceMailStreamEntry mail = new AllianceMailStreamEntry();
+                    mail.SetId((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
+                    mail.SetSenderId(0);
+                    mail.SetSenderAvatarId(0);
+                    mail.SetSenderName("Server Status Boy");
+                    mail.SetIsNew(0);
+                    mail.SetAllianceId(0);
+                    mail.SetAllianceBadgeData(0);
+                    mail.SetAllianceName("Server Status Group");
+                    mail.SetMessage(message + "Connected Players:" + ResourcesManager.GetConnectedClients().Count + "\nMore Coming Soon");
+                    mail.SetSenderLeagueId(22);
+
+                    foreach (var onlinePlayer in ResourcesManager.GetOnlinePlayers())
+                    {
+                        var p = new AvatarStreamEntryMessage(onlinePlayer.GetClient());
+                        p.SetAvatarStreamEntry(mail);
+                        PacketManager.ProcessOutgoingPacket(p);
+                    }
+                }
+            }
+            else
+            {
+                SendCommandFailedMessage(level.GetClient());
+            }
+        }
+    }
+}
+
+//Made by Julian / iSwuerfel
